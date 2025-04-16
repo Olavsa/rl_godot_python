3ee1a20ac4f871ed3f428529e5e9580ebd8aedb2
Olavsa <115793802+Olavsa@users.noreply.github.com>
2025-04-16 17:17:30 +0200

    Add tensorboard to parking environment

--- python/sb3_agent_parking3d.py
+++ python/sb3_agent_parking3d.py
@@ -1,10 +1,14 @@
 import os
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.monitor import Monitor
 from parking_env import ParkingEnv  # Ensure you import your environment correctly
 
 # Create the environment with rendering enabled
 env = ParkingEnv(render_mode="human")
+
+# Wrap with monitor for logging
+env = Monitor(env, "./python/ppo_parking_tensorboard/", allow_early_resets=True)
 
 # Check the environment for compatibility
 #check_env(env, warn=True)
@@ -16,13 +20,20 @@ model_path = f"{model_file_name}.zip"
 # Load existing model if available, otherwise initialize a new one
 if os.path.exists(model_path):
     print("Loading existing model...")
-    model = PPO.load(model_path, env=env)  # Load with the same environment
+    model = PPO.load(model_path, env=env, tensorboard_log="./python/ppo_parking_tensorboard/")  # Load with the same environment
 else:
     print("No existing model found. Creating new one...")
-    model = PPO("MultiInputPolicy", env, verbose=1)
+    model = PPO("MultiInputPolicy", env, verbose=1, tensorboard_log="./python/ppo_parking_tensorboard/")
 
-num_iterations = 100
-timesteps_per_iter = 2048 * 15
+tensorboard_log_dir = "./python/ppo_parking_tensorboard/"
+if not os.path.exists(tensorboard_log_dir):
+    os.makedirs(tensorboard_log_dir)
+    print(f"Created log directory: {tensorboard_log_dir}")
+else:
+    print(f"Log directory already exists: {tensorboard_log_dir}")
+
+num_iterations = 30
+timesteps_per_iter = 2048 * 5
 for i in range(num_iterations):
     # Train the model (continue training if loaded)
     print(f"Training iteration {i+1}/{num_iterations}...")
