e3c9f2291ea8fb615a14a77f1fab2db1c538f6ea
Olavsa <115793802+Olavsa@users.noreply.github.com>
2025-04-16 13:12:37 +0200

    Update parking environment observation space
    Update gym environment for parking simulator to reflect new lidar sensors and  adding a parameter for car rotation

--- python/parking_env.py
+++ python/parking_env.py
@@ -21,8 +21,10 @@ class ParkingEnv(gym.Env):
         self._speed = 0.0
         self._car_pos = np.array([0.0, 0.0])
         self._target_pos = np.array([0.0, 0.0])
-        self._sensors_front = np.array([20.0, 20.0, 20.0, 20.0, 20.0])
-        self._sensors_back = np.array([20.0, 20.0, 20.0, 20.0, 20.0])
+        self._sensors_front = np.array([20.0] * 13)
+        self._sensors_back = np.array([20.0] * 13)
+        self._sensors_middle = np.array([20.0] * 6)
+        self._rotation = np.array([0.0]*3)
         self._reward = 0.0
         self._truncated = 0
         self._done = 0
@@ -33,8 +35,10 @@ class ParkingEnv(gym.Env):
                 "speed": gym.spaces.Box(low=0.0, high=100.0, shape=(1,), dtype=np.float32),
                 "car_pos": gym.spaces.Box(low=-np.inf, high=np.inf, shape=(2,), dtype=np.float32),
                 "target_pos": gym.spaces.Box(low=-np.inf, high=np.inf, shape=(2,), dtype=np.float32),
-                "sensors_front": gym.spaces.Box(low=0.0, high=20.0, shape=(5,), dtype=np.float32),
-                "sensors_back": gym.spaces.Box(low=0.0, high=20.0, shape=(5,), dtype=np.float32),
+                "sensors_front": gym.spaces.Box(low=0.0, high=20.0, shape=(13,), dtype=np.float32),
+                "sensors_back": gym.spaces.Box(low=0.0, high=20.0, shape=(13,), dtype=np.float32),
+                "sensors_middle": gym.spaces.Box(low=0.0, high=20.0, shape=(6,), dtype=np.float32),
+                "rotation": gym.spaces.Box(low=-1.0, high=1.0, shape=(3,), dtype=np.float32),
                 "reward": gym.spaces.Box(low=-15, high=15, shape=(1,), dtype=np.float32),
                 "truncated" : gym.spaces.Discrete(2),
                 "done": gym.spaces.Discrete(2)
@@ -53,8 +57,10 @@ class ParkingEnv(gym.Env):
             "speed": np.array([self._speed], dtype=np.float32),  # (1,)
             "car_pos": self._car_pos.astype(np.float32),         #  (2,)
             "target_pos": self._target_pos.astype(np.float32),   #  (2,)
-            "sensors_front": self._sensors_front.astype(np.float32),  #  (5,)
-            "sensors_back": self._sensors_back.astype(np.float32),    #  (5,)
+            "sensors_front": self._sensors_front.astype(np.float32),  #  (13,)
+            "sensors_back": self._sensors_back.astype(np.float32),    #  (13,)
+            "sensors_middle": self._sensors_middle.astype(np.float32),    #  (6,)
+            "rotation": self._rotation.astype(np.float32),    #  (3,)
             "reward": np.array([self._reward], dtype=np.float32),     #  (1,)
             "truncated": int(self._truncated),
             "done": int(self._done)
@@ -71,9 +77,11 @@ class ParkingEnv(gym.Env):
         self._target_pos = np.array(observation_dict["p3"], dtype=np.float32)
         self._sensors_front = np.array(observation_dict["p4"], dtype=np.float32)
         self._sensors_back = np.array(observation_dict["p5"], dtype=np.float32)
-        self._reward = observation_dict.get("p6", 0)
-        self._truncated = observation_dict.get("p7", 0)
-        self._done = observation_dict.get("p8", 0)
+        self._sensors_middle = np.array(observation_dict["p6"], dtype=np.float32)
+        self._rotation = np.array(observation_dict["p7"], dtype=np.float32)
+        self._reward = observation_dict.get("p8", 0)
+        self._truncated = observation_dict.get("p9", 0)
+        self._done = observation_dict.get("p10", 0)
         
     
     
