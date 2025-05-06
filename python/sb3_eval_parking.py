f125d9e3b0024a7867ea8fb7f961a10e63218cbe
Olavsa <115793802+Olavsa@users.noreply.github.com>
2025-05-06 13:50:05 +0200

    Update sb3 eval for parking
    Added a small print statement with average reward, parking success rate etc. for parking envrionment

--- python/sb3_eval_parking.py
+++ python/sb3_eval_parking.py
@@ -19,7 +19,10 @@ model_path = f"./python/ppo_parking/trained_models/{model_file_name}.zip"
 model = PPO.load(model_path, env=eval_env)
 
 # Evaluate the model deterministically (i.e., no entropy/randomness)
-n_eval_episodes = 10
+n_eval_episodes = 50
+
+# Episode data
+rewards = []
 
 for ep in range(n_eval_episodes):
     obs = eval_env.reset()
@@ -32,4 +35,18 @@ for ep in range(n_eval_episodes):
         obs, reward, done, info = eval_env.step(action)
         total_reward += reward
         steps += 1
+        
+    rewards.append(total_reward.item())
 eval_env.close()
+
+
+parking_success = sum(rew > 20 for rew in rewards)
+success_rate = parking_success / n_eval_episodes * 100
+
+# Simple eval print
+print(f"Episodes run: {n_eval_episodes}")
+print(f"\tEpisode reward mean: {np.mean(rewards):.2f}")
+print(f"\tMax episode reward:  {np.max(rewards):.2f}")
+print(f"\tMin episode reward:  {np.min(rewards):.2f}")
+print(f"\tParked: {success_rate:.1f}% ({parking_success}/{n_eval_episodes})")
+print(f"\tEP rewards (sorted): {sorted([round(rew, 1) for rew in rewards])}")
