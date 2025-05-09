ea14c38ce0c5bfe3f0836c96f26e6164c0f6b4fc
Olavsa <115793802+Olavsa@users.noreply.github.com>
2025-05-09 18:08:27 +0200

    Add option to run random inputs for parking env
    Separated the random inputs section of the random inputs test into its own method.

--- rl_godot/game/game_envs/parking_simulator_3d/Scenes/Levels/level_2.gd
+++ rl_godot/game/game_envs/parking_simulator_3d/Scenes/Levels/level_2.gd
@@ -28,7 +28,6 @@ func set_random_target_spot():
 	var rand_spot_nr = randi_range(0, parking_spots.size() - 1)
 	
 	set_target_spot(parking_spots[rand_spot_nr])
-	#print("target: ", str(rand_spot_nr+1))
 	
 
 func set_target_spot(parking_spot: ParkingSpot):
@@ -38,11 +37,8 @@ func set_target_spot(parking_spot: ParkingSpot):
 	parking_spot.set_as_target()
 	target_spot = parking_spot
 
-func test():
-	# Seed=123 with render and 8x speed -> 		 Position after 6000 ticks: (5.441027, 0.023884, 18.48198)
-	# Seed 123 withhout render and 200x speed -> Position after 6000 ticks: (5.441027, 0.023884, 18.48198)
 
-	var physics_ticks = Engine.get_physics_frames()
+func run_rand_input_action():
 	var r = rnd.randi_range(0, 5)
 	if r == 0:
 		Input.action_press("ui_up")
@@ -56,8 +52,50 @@ func test():
 	if r == 3:
 		Input.action_press("ui_left")
 		Input.action_release("ui_right")
+		
 
+func test():
+	# Seed=123 with render and 8x speed -> 		 Position after 6000 ticks: (5.441027, 0.023884, 18.48198)
+	# Seed 123 withhout render and 200x speed -> Position after 6000 ticks: (5.441027, 0.023884, 18.48198)
+	run_rand_input_action()
+	var physics_ticks = Engine.get_physics_frames()
 	const base_fps = 60
 	const time_before_check_pos = 20
 	if physics_ticks % (base_fps * time_before_check_pos) == 0:
-		print("Position after "+ str(physics_ticks) + " ticks: %s" % str($hatchback_car.global_position))
+		print("Position after "+ str(physics_ticks) + " ticks: %s" % str($hatchback_car.global_position))		
+
+
+# 1x with rendering
+#Position after 1200 ticks: (23.80098, 0.022456, -3.81862)
+#Position after 2400 ticks: (24.53352, 0.027196, -2.218262)
+#Position after 3600 ticks: (17.78854, 0.029036, 5.794172)
+#Position after 4800 ticks: (19.8131, 0.029738, 7.954369)
+#Position after 6000 ticks: (23.4629, 0.02725, 16.91899)
+#Position after 18000 ticks:(-6.77454, 0.027036, 8.081354)
+
+
+# 8x with rendering
+#Position after 1200 ticks: (23.80098, 0.022456, -3.81862)
+#Position after 2400 ticks: (24.53352, 0.027196, -2.218262)
+#Position after 3600 ticks: (17.78854, 0.029036, 5.794172)
+#Position after 4800 ticks: (19.8131, 0.029738, 7.954369)
+#Position after 6000 ticks: (23.4629, 0.02725, 16.91899)
+#Position after 18000 ticks: (-6.77454, 0.027036, 8.081354)
+
+# 16x with rendering
+#Position after 1200 ticks: (23.80098, 0.022456, -3.81862)
+#Position after 2400 ticks: (24.53352, 0.027196, -2.218262)
+#Position after 3600 ticks: (17.78854, 0.029036, 5.794172)
+#Position after 4800 ticks: (19.8131, 0.029738, 7.954369)
+#Position after 6000 ticks: (23.4629, 0.02725, 16.91899)
+#Position after 18000 ticks: (-6.77454, 0.027036, 8.081354)
+
+
+
+#50x without rendering
+#Position after 1200 ticks: (23.80098, 0.022456, -3.81862)
+#Position after 2400 ticks: (24.53352, 0.027196, -2.218262)
+#Position after 3600 ticks: (17.78854, 0.029036, 5.794172)
+#Position after 4800 ticks: (19.8131, 0.029738, 7.954369)
+#Position after 6000 ticks: (23.4629, 0.02725, 16.91899)
+#Position after 18000 ticks: (-6.77454, 0.027036, 8.081354)
