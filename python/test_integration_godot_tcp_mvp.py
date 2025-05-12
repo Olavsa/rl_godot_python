516660e7d33e807a7c643c0595bf213f19575712
Olavsa <115793802+Olavsa@users.noreply.github.com>
2025-05-12 22:48:31 +0200

    Add integration test with gym env as interface

--- python/test_integration_godot_tcp_mvp.py
+++ python/test_integration_godot_tcp_mvp.py
@@ -1,7 +1,8 @@
 import time
 from tcp.sync_tcp_client import SyncTCPClient
+from godot_gym_envs.game_env import GameEnv
 
-def get_action(observation_dict):
+def get_action_tcp_test(observation_dict):
     """
     Dummy action logic based on observations from Godot.
     Agent should get to the goal with these actions."""
@@ -16,8 +17,57 @@ def get_action(observation_dict):
         return (0, 0)  # Stop moving
     else:
         return (0, 1)  # Move right
+    
+def get_action_gym_test(observation_dict):
+    """
+    Dummy action logic based on observations from Godot and gymnasium.
+    Agent should get to the goal with these actions."""
+    
+    dist_to_obstacle = observation_dict["distance_to_box"]
 
-def test_agent():
+    if 0 <= dist_to_obstacle <= 150:
+        return 1  # Jump and move right
+    else:
+        return 0  # Move right
+
+
+def test_agent_gym_env():
+    """Tests agent behavior with gym environment."""
+    env = GameEnv("human")
+    
+    episode_count = 0
+    max_episodes = 1  # Run multiple episodes
+
+    while episode_count < max_episodes:
+        print(f"\n\n==== Starting Episode {episode_count+1} ====")
+
+        # Reset environment at the start of each episode
+        observation = env.reset()[0]
+        print(f"Initial Observation: {observation}")
+
+        step_count = 0
+        done = False
+        while not done:
+            action = get_action_gym_test(observation)
+            print(f"Step {step_count+1}: Sending action {action}")
+            
+            observation = env.step(action)[0]
+            done = observation["is_finished"]
+            print(f"Observation: {observation}, Done: {done}")
+
+            if done:
+                print("Game finished! Resetting environment...")
+                break  # Reset next loop iteration
+
+            step_count += 1
+
+        episode_count += 1
+
+    print("Test completed successfully.")
+
+
+
+def test_agent_tcp():
     """Tests agent behavior with reset support in Godot."""
     
     with SyncTCPClient() as client:
@@ -36,7 +86,7 @@ def test_agent():
             step_count = 0
             done = False
             while not done:
-                action = get_action(observation)
+                action = get_action_tcp_test(observation)
                 print(f"Step {step_count+1}: Sending action {action}")
                 
                 observation = client.step(action)
@@ -54,4 +104,5 @@ def test_agent():
     print("Test completed successfully.")
 
 if __name__ == "__main__":
-    test_agent()
+    test_agent_tcp()
+    #test_agent_gym_env()
